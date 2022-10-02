56d0fd715338d2a5bac56788e72d38a7de1f3f53
Haris Avdicevic <haris.avdi93@gmail.com>
2022-10-02 17:11:16 +0200

    Fixed so that Prev & next buttons only renders if there are more pages left

--- src/App.js
+++ src/App.js
@@ -49,8 +49,8 @@ function App() {
                 characters={characters}
             />
             <Pagination
-            goToNextPage={goToNextPage}
-            goToPrevPage={goToPrevPage}
+            goToNextPage={nextPageUrl ? goToNextPage : null}
+            goToPrevPage={prevPageUrl ? goToPrevPage: null}
             />
         </div>
     );
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,8 +3,8 @@ import React from "react";
 export const Pagination = ({goToNextPage, goToPrevPage}) => {
     return (
         <div>
-            <button onClick={goToPrevPage}>Previous</button>
-            <button onClick={goToNextPage}>Next</button>
+            {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
+            {goToNextPage && <button onClick={goToNextPage}>Next</button>}
         </div>
     )
 }

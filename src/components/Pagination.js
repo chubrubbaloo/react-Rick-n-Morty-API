6e3a50e6d3d60eae929a280160a0610b43e721cd
Haris Avdicevic <haris.avdi93@gmail.com>
2022-10-02 21:24:44 +0200

    Auto scroll to the top while using pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,8 +3,22 @@ import React from "react";
 export const Pagination = ({goToNextPage, goToPrevPage}) => {
     return (
         <div>
-            {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
-            {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+            {goToPrevPage && <button onClick={() => {
+                window.scroll({
+                    top:0,
+                    left: 0,
+                    behavior: 'smooth'
+                })
+                goToPrevPage()
+            }}>Previous</button>}
+            {goToNextPage && <button onClick={() => {
+                window.scroll({
+                    top:0,
+                    left: 0,
+                    behavior: 'smooth'
+                })
+                goToNextPage()
+            }}>Next</button>}
         </div>
     )
 }

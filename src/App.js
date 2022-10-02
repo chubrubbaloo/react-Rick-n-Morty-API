b4426ff847a5367231f194dbae00b39ddbd8d30d
Haris Avdicevic <haris.avdi93@gmail.com>
2022-10-02 09:16:48 +0200

    Added cancel token for requests

--- src/App.js
+++ src/App.js
@@ -12,28 +12,32 @@ function App() {
     const [prevPageUrl, setPrevPageUrl] = useState()
     const [loading, setLoading] = useState(true);
 
-    
+
     useEffect(() => {
         setLoading(true)
+        let cancel;
         const fetchItems = async () => {
 
-            const result = await axios.get(currentPageUrl);
+            const result = await axios.get(currentPageUrl,
+                {cancelToken: new axios.CancelToken(c => cancel = c)});
             setLoading(false);
             setNextPageUrl(result.data.next)
             setPrevPageUrl(result.data.prev)
             setCharacters(result.data.results)
         }
         fetchItems();
+        // Cancels old request when we make a new one so we never load old data in case the old request finishes after the new one.
+        return () => cancel();
 
-    },[currentPageUrl])
+    }, [currentPageUrl])
 
-    if (loading) return 'Loading...'
+    if (loading) return <h1>Loading...</h1>
 
 
     return (
         <div className="App">
             <Logo/>
-            <Characters characters={characters} />
+            <Characters characters={characters}/>
         </div>
     );
 }

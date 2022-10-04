fd3e4f6c2281d51c18580da3d75e2a1efdeac243
Haris Avdicevic <haris.avdi93@gmail.com>
2022-10-04 21:39:29 +0200

    Fixed scroll bug

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,18 +4,13 @@ export const Pagination = ({goToNextPage, goToPrevPage}) => {
     return (
         <div>
             {goToPrevPage && <button onClick={() => {
-                window.scroll({
-                    top:0,
-                    left: 0,
-                    behavior: 'smooth'
-                })
+                window.top()
                 goToPrevPage()
             }}>Previous</button>}
             {goToNextPage && <button onClick={() => {
                 window.scroll({
                     top:0,
                     left: 0,
-                    behavior: 'smooth'
                 })
                 goToNextPage()
             }}>Next</button>}

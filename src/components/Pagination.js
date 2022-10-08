818adb862f33a09d9a465a2a031495ff5653d8fb
Haris Avdicevic <haris.avdi93@gmail.com>
2022-10-08 13:38:59 +0200

    Fix: auto-scroll to top for all buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,25 +5,21 @@ export const Pagination = ({goToNextPage, goToPrevPage, goToPage, pages}) => {
 
     const pageButtons = []
     for (let i = 1; i <= pages; i++) {
-        pageButtons.push(<button className='page-flipper'  key={i} onClick={() => goToPage(i)}>{i}</button>)
+        pageButtons.push(<button className='page-flipper'  key={i} onClick={() => {
+            window.scroll({top:0,left:0})
+            goToPage(i)
+        }}>{i}</button>)
     }
 
     return (
         <div id='page-flipper-container'>
-
             {goToPrevPage && <button className='page-flipper' onClick={() => {
-                window.scroll({
-                    top: 0,
-                    left: 0
-                })
+                window.scroll({ top: 0,left: 0})
                 goToPrevPage()
             }}><GiPreviousButton id='left-arrow'/> Prev </button>}
             {pageButtons}
             {goToNextPage && <button className='page-flipper' onClick={() => {
-                window.scroll({
-                    top: 0,
-                    left: 0,
-                })
+                window.scroll({top: 0,left: 0,})
                 goToNextPage()
             }}>Next <GiNextButton id='right-arrow'/></button>}
         </div>
